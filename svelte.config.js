afe20d7dc2c6e9c12b2fa3e646154e245c766050
Tengku Ahmad Firdaus B Tengku Kamaruddin <firdaus.kamaruddin@sitecore.com>
2024-04-07 22:11:55 +0800

    add blog from old site

--- svelte.config.js
+++ svelte.config.js
@@ -10,9 +10,9 @@ const mdsvexOptions = {
 		highlighter: async (code, lang = 'text') => {
 			const highlighter = await getHighlighter({
 				themes: ['poimandres'],
-				langs: ['javascript', 'typescript', 'json', 'shell', "c#", "xml"]
+				langs: ['javascript', 'typescript', 'json', 'shell', "c#", "xml", "gherkin"]
 			})
-			await highlighter.loadLanguage('javascript', 'typescript', 'json', 'shell', "c#", "xml")
+			await highlighter.loadLanguage('javascript', 'typescript', 'json', 'shell', "c#", "xml", "gherkin")
 			const html = escapeSvelte(highlighter.codeToHtml(code, { lang, theme: 'poimandres' }))
 			return `{@html \`${html}\` }`
 		}
